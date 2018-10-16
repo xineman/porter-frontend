73f5418e9d389285e31bcada0900008844965bfe
xineman <uragupalo@mail.ru>
2018-10-16 19:21:36 +0300

    Fix 'build' npm script, add bundle size visualizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,15 +2,16 @@ const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const Visualizer = require('webpack-visualizer-plugin');
 const common = require('./webpack.common.js');
 
 const {
-  DIST_PATH
+  DIST_PATH,
 } = require('./config/paths');
 const {
   cssLoader,
   sassLoader,
-  postcssLoader
+  postcssLoader,
 } = require('./config/loaders');
 
 
@@ -26,20 +27,20 @@ module.exports = merge(common, {
               MiniCssExtractPlugin.loader,
               'css-loader',
               postcssLoader,
-              sassLoader
-            ]
+              sassLoader,
+            ],
           },
           {
             use: [
               MiniCssExtractPlugin.loader,
               cssLoader,
               postcssLoader,
-              sassLoader
-            ]
-          }
-        ]
-      }
-    ]
+              sassLoader,
+            ],
+          },
+        ],
+      },
+    ],
   },
 
   devtool: 'source-map',
@@ -48,19 +49,21 @@ module.exports = merge(common, {
     path: DIST_PATH,
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].js',
-    sourceMapFilename: '[name].[chunkhash].js.map'
+    sourceMapFilename: '[name].[chunkhash].js.map',
   },
 
   plugins: [
     new UglifyJSPlugin({
       uglifyOptions: {
-        ecma: 7
+        ecma: 7,
       },
-      sourceMap: true
+      sourceMap: true,
     }),
 
     new MiniCssExtractPlugin({ filename: 'main.[chunkhash].css' }),
 
-    new Dotenv()
-  ]
+    new Dotenv(),
+
+    new Visualizer(),
+  ],
 });
